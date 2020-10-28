1b1d48af2a681476677a0531cffe89c820217aca
Albert Morales <albert.morales@upr.edu>
2020-10-27 23:22:05 -0400

    Pet User Mode; relation

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pet extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+//    public function petType(){
+//        return $this->belongsTo(PetType::class);
+//    }
 }
