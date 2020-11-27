7d559ee7b4a6d84c48f905e110efdb5b9b2bf2c5
Albert Morales <albert.morales@upr.edu>
2020-11-27 14:11:45 -0400

    Update 2014_10_11_000000_create_roles_table.php

--- database/migrations/2014_10_11_000000_create_roles_table.php
+++ database/migrations/2014_10_11_000000_create_roles_table.php
@@ -13,6 +13,7 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key=0');
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
